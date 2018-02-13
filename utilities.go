cf68629b3dd11fb871854fe91e335ac77a04b651
Michael <michael@Michaels-MacBook-Pro.local>
2018-02-13 16:22:42 +0800

    Update simple controller tps limit. move threshold value ouside control()

--- utilities.go
+++ utilities.go
@@ -1,42 +1,48 @@
 package nimo
 
-import "time"
-
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+)
 
 type RateController interface {
 	// take one from controller. if no more in the
 	// pool (or tokens). make it block.
 	//
 	// return true. if need to be blocked
-	Control() bool
+	Control(threshold int64) bool
 }
 
 // SimpleRateController. simply increase a inner counter
 // and block if the counter grew up to it's threshold and
 // yield current routine for a while
 type SimpleRateController struct {
-	Threshold, token int64
-	tick             int64
+	token int64
+	tick  int64
+
+	lock *sync.Mutex
 }
 
 func NewSimpleRateController() *SimpleRateController {
-	return &SimpleRateController{tick: time.Now().Unix()}
+	return &SimpleRateController{tick: time.Now().Unix(), lock: new(sync.Mutex)}
 }
 
-func (controller *SimpleRateController) SetThreshold(threshold int64) {
-	controller.Threshold = threshold
-}
-
-func (controller *SimpleRateController) Control() bool {
+func (controller *SimpleRateController) Control(threshold int64) bool {
 	now := time.Now().Unix()
 	// current second is forward. we are behind
 	if now > controller.tick {
-		controller.tick = now
-		controller.token = 0
+		controller.lock.Lock()
+		// check it again
+		if now > controller.tick {
+			controller.tick = now
+			controller.token = 0
+		}
+		controller.lock.Unlock()
 		return false
 	}
 	AssertTrue(now == controller.tick, "rate controller tick now correct !")
 
-	controller.token++
-	return controller.token >= controller.Threshold
+	snapshot := atomic.AddInt64(&controller.token, 1)
+	return snapshot >= threshold
 }
